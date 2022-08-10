89f0cc85681a5099e632ec921c148cbc783ab7f7
Evan Gray <battledingo@gmail.com>
2022-08-10 14:37:53 -0400

    dashboard: alert cleanup

--- dashboard/src/components/Alerts.tsx
+++ dashboard/src/components/Alerts.tsx
@@ -1,29 +1,42 @@
-import { Alert } from "@mui/material";
+import { Alert, AlertColor } from "@mui/material";
 import { ChainIdToHeartbeats } from "../hooks/useChainHeartbeats";
 import chainIdToName from "../utils/chainIdToName";
+
+type AlertEntry = {
+  severity: AlertColor;
+  text: string;
+};
 
 function Alerts({
   chainIdsToHeartbeats,
 }: {
   chainIdsToHeartbeats: ChainIdToHeartbeats;
 }) {
-  const alerts: string[] = [];
+  const alerts: AlertEntry[] = [];
+  const downChains: { [chainId: string]: string[] } = {};
   Object.entries(chainIdsToHeartbeats).forEach(([chainId, heartbeats]) => {
     heartbeats.forEach((heartbeat) => {
       if (heartbeat.network.height === "0") {
-        alerts.push(
-          `${heartbeat.name} is down on ${chainIdToName(
-            Number(chainId)
-          )} (${chainId})!`
-        );
+        if (!downChains[chainId]) {
+          downChains[chainId] = [];
+        }
+        downChains[chainId].push(heartbeat.name);
       }
+    });
+  });
+  Object.entries(downChains).forEach(([chainId, names]) => {
+    alerts.push({
+      severity: names.length >= 7 ? "error" : "warning",
+      text: `${names.join(", ")} ${
+        names.length > 1 ? "are" : "is"
+      } down on ${chainIdToName(Number(chainId))} (${chainId})!`,
     });
   });
   return (
     <>
       {alerts.map((alert) => (
-        <Alert key={alert} severity="error">
-          {alert}
+        <Alert key={alert.text} severity={alert.severity}>
+          {alert.text}
         </Alert>
       ))}
     </>

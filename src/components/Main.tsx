064af6bc38b266d1533950150a7a7c044a0d996a
Evan Gray <battledingo@gmail.com>
2022-08-11 23:09:34 -0400

    Make panels collapsible #13

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,33 +1,70 @@
-import { Box, Divider, Typography } from "@mui/material";
-import Alerts from "./Alerts";
-import Chains from "./Chains";
-import Guardians from "./Guardians";
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Divider,
+  Typography,
+} from "@mui/material";
+import { ReactNode } from "react";
 import useChainHeartbeats from "../hooks/useChainHeartbeats";
 import useHeartbeats from "../hooks/useHeartbeats";
+import Alerts from "./Alerts";
+import Chains from "./Chains";
 import Governor from "./Governor";
+import Guardians from "./Guardians";
+
+function CollapsibleSection({
+  header,
+  children,
+}: {
+  header: ReactNode;
+  children: ReactNode;
+}) {
+  return (
+    <Accordion
+      defaultExpanded
+      disableGutters
+      sx={{
+        background: "transparent",
+        my: 0.5,
+        "&:not(:last-child)": {
+          borderBottom: 0,
+        },
+        "&:before": {
+          display: "none",
+        },
+      }}
+    >
+      <AccordionSummary expandIcon={<ExpandMore />}>
+        <Typography variant="h5">{header}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>{children}</AccordionDetails>
+    </Accordion>
+  );
+}
+
 function Main() {
   const heartbeats = useHeartbeats();
   const chainIdsToHeartbeats = useChainHeartbeats(heartbeats);
   return (
     <>
-      <Alerts chainIdsToHeartbeats={chainIdsToHeartbeats} />
-      <Divider />
-      <Box pt={2} pl={2}>
-        <Typography variant="h5">Guardians</Typography>
-      </Box>
       <Box p={2}>
+        <Alerts chainIdsToHeartbeats={chainIdsToHeartbeats} />
+      </Box>
+      <Divider />
+      <CollapsibleSection header="Guardians">
         <Guardians heartbeats={heartbeats} />
-      </Box>
+      </CollapsibleSection>
       <Divider />
-      <Box pt={2} pl={2}>
-        <Typography variant="h5">Governor</Typography>
-      </Box>
-      <Governor />
+      <CollapsibleSection header="Governor">
+        <Governor />
+      </CollapsibleSection>
       <Divider />
-      <Box pt={2} pl={2}>
-        <Typography variant="h5">Chains</Typography>
-      </Box>
-      <Chains chainIdsToHeartbeats={chainIdsToHeartbeats} />
+      <CollapsibleSection header="Chains">
+        <Chains chainIdsToHeartbeats={chainIdsToHeartbeats} />
+      </CollapsibleSection>
     </>
   );
 }

163c7f530e7a8e388af5da6dcddc4a11eda80e56
Evan Gray <battledingo@gmail.com>
2022-08-28 14:09:52 -0400

    dashboard: fixes #21 save settings to localStorage

--- dashboard/src/contexts/SettingsContext.tsx
+++ dashboard/src/contexts/SettingsContext.tsx
@@ -2,23 +2,56 @@ import React, {
   ReactNode,
   useCallback,
   useContext,
+  useEffect,
   useMemo,
   useState,
 } from "react";
 
+const STORAGE_KEY = "settings";
+
 export type Theme = "light" | "dark" | "auto";
 
 type Settings = {
+  defaultEndpoint?: string;
   theme: Theme;
 };
 
 type SettingsContextValue = {
   settings: Settings;
+  updateDefaultEndpoint(value: string): void;
   updateTheme(value: Theme): void;
 };
 
+const isTheme = (arg: any): arg is Theme => {
+  return arg && (arg === "light" || arg === "dark" || arg === "auto");
+};
+
+const isSettings = (arg: any): arg is Settings => {
+  return arg && arg.theme && isTheme(arg.theme);
+};
+
+let localStorageSettings: Settings | null = null;
+try {
+  const value = localStorage.getItem(STORAGE_KEY);
+  if (value) {
+    const parsedValue = JSON.parse(value);
+    if (isSettings(parsedValue)) {
+      localStorageSettings = parsedValue;
+    }
+  }
+} catch (e) {}
+
+const initialSettings: Settings = localStorageSettings || { theme: "auto" };
+
+const saveSettings = (settings: Settings) => {
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(settings));
+  } catch (e) {}
+};
+
 const SettingsContext = React.createContext<SettingsContextValue>({
-  settings: { theme: "auto" },
+  settings: initialSettings,
+  updateDefaultEndpoint: (value: string) => {},
   updateTheme: (value: Theme) => {},
 });
 
@@ -27,13 +60,20 @@ export const SettingsContextProvider = ({
 }: {
   children: ReactNode;
 }) => {
-  const [settings, setSettings] = useState<Settings>({ theme: "auto" });
+  const [settings, setSettings] = useState<Settings>(initialSettings);
+  const updateDefaultEndpoint = useCallback((value: string) => {
+    setSettings((settings) => ({ ...settings, defaultEndpoint: value }));
+  }, []);
   const updateTheme = useCallback((value: Theme) => {
     setSettings((settings) => ({ ...settings, theme: value }));
   }, []);
+  // sync settings to state
+  useEffect(() => {
+    saveSettings(settings);
+  }, [settings]);
   const value = useMemo(
-    () => ({ settings, updateTheme }),
-    [settings, updateTheme]
+    () => ({ settings, updateDefaultEndpoint, updateTheme }),
+    [settings, updateDefaultEndpoint, updateTheme]
   );
   return (
     <SettingsContext.Provider value={value}>

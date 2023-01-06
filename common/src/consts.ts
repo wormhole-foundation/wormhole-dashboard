3ab860f2d25f09c28388d75c7d882b505eeda9f9
Evan Gray <battledingo@gmail.com>
2023-01-06 20:51:32 +0000

    common: add cosmwasm starting blocks

--- common/src/consts.ts
+++ common/src/consts.ts
@@ -4,6 +4,7 @@ export const INITIAL_DEPLOYMENT_BLOCK_BY_CHAIN: {
   [key in ChainName]?: string;
 } = {
   ethereum: '12959638',
+  terra: '4810000', // not sure exactly but this should be before the first known message
   bsc: '9745450',
   polygon: '20629146',
   avalanche: '8237163',
@@ -15,7 +16,10 @@ export const INITIAL_DEPLOYMENT_BLOCK_BY_CHAIN: {
   klaytn: '90563824',
   celo: '12947144',
   moonbeam: '1486591',
+  terra2: '399813',
+  injective: '20908376',
   arbitrum: '18128584',
   aptos: '0', // block is 1094390 but AptosWatcher uses sequence number instead
   near: '72767136',
+  xpla: '777549',
 };

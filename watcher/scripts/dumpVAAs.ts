7b75a7e2d73f98951508564da9b3dda54ed230c8
Evan Gray <battledingo@gmail.com>
2023-02-09 16:51:36 +0000

    scripts: add dumpTransferData

--- watcher/scripts/dumpVAAs.ts
+++ watcher/scripts/dumpVAAs.ts
@@ -1,16 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+import { CHAINS, coalesceChainName } from '@certusone/wormhole-sdk/lib/cjs/utils/consts';
+import { padUint16 } from '@wormhole-foundation/wormhole-monitor-common';
 import { appendFileSync, closeSync, openSync } from 'fs';
-import { BigtableDatabase } from '../src/databases/BigtableDatabase';
-import { assertEnvironmentVariable } from '@wormhole-foundation/wormhole-monitor-common';
-import {
-  CHAINS,
-  CHAIN_ID_PYTHNET,
-  CHAIN_ID_UNSET,
-  coalesceChainName,
-} from '@certusone/wormhole-sdk/lib/cjs/utils/consts';
 import ora from 'ora';
-import { BigtableVAAsResultRow } from '../src/databases/types';
+import { BigtableDatabase } from '../src/databases/BigtableDatabase';
+import { BigtableSignedVAAsResultRow } from '../src/databases/types';
 
 // This script dumps all VAAs to a csv file compatible with the guardian `sign-existing-vaas-csv` admin command
 
@@ -23,41 +18,30 @@ const LIMIT = 10000;
     if (!bt.bigtable) {
       throw new Error('bigtable is undefined');
     }
-    const vaaTableId = assertEnvironmentVariable('BIGTABLE_VAA_TABLE_ID');
     const instance = bt.bigtable.instance(bt.instanceId);
-    const vaaTable = instance.table(vaaTableId);
-    const filteredChainIds = Object.values(CHAINS).filter(
-      (c) => c !== CHAIN_ID_PYTHNET && c !== CHAIN_ID_UNSET
-    );
-    const filter = [
-      {
-        family: 'QuorumState',
-        column: 'SignedVaa',
-      },
-    ];
-    for (const chain of filteredChainIds) {
+    const vaaTable = instance.table(bt.signedVAAsTableId);
+    for (const chain of Object.values(CHAINS)) {
       const chainName = coalesceChainName(chain);
       let total = 0;
       let log = ora(`Fetching all ${chainName} VAAs...`).start();
-      let start = `${chain}:`;
+      let start = `${padUint16(chain.toString())}/`;
       while (start) {
         log.text = `Fetching ${LIMIT}/${total} ${chainName} VAAs starting at ${start}...`;
         let vaaRows = (
           await vaaTable.getRows({
             start,
-            end: `${chain}:z`,
+            end: `${padUint16(chain.toString())}/z`,
             decode: false,
-            filter,
             limit: LIMIT,
           })
-        )[0] as BigtableVAAsResultRow[];
+        )[0] as BigtableSignedVAAsResultRow[];
         start = vaaRows.length === LIMIT ? vaaRows[LIMIT - 1].id : '';
         vaaRows = vaaRows.filter((row) => row.id.toString() !== start.toString());
         total += vaaRows.length;
         log.text = `Processing ${total} ${chainName} VAAs...`;
         for (const row of vaaRows) {
           try {
-            const vaaBytes = row.data.QuorumState.SignedVAA?.[0].value;
+            const vaaBytes = row.data.info.bytes?.[0].value;
             if (vaaBytes) {
               appendFileSync(fd, `${row.id},${vaaBytes.toString('hex')}\n`, 'utf8');
             }

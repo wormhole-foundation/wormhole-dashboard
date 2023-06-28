971c6d894e6ef00dbe76d50daef432e56d6d1fdd
Paul Noel <panoel007@gmail.com>
2023-06-28 13:53:16 +0000

    watcher: optimism watcher update

--- watcher/src/watchers/__tests__/OptimismWatcher.test.ts
+++ watcher/src/watchers/__tests__/OptimismWatcher.test.ts
@@ -1,33 +1,32 @@
 import { expect, jest, test } from '@jest/globals';
 import { INITIAL_DEPLOYMENT_BLOCK_BY_CHAIN } from '@wormhole-foundation/wormhole-monitor-common';
-import { OptimismWatcher } from '../OptimismWatcher';
+import { EVMWatcher } from '../EVMWatcher';
 
 jest.setTimeout(60000);
 
 const initialOptimismBlock = Number(INITIAL_DEPLOYMENT_BLOCK_BY_CHAIN.optimism);
 
 test('getFinalizedBlockNumber', async () => {
-  const watcher = new OptimismWatcher();
+  const watcher = new EVMWatcher('optimism');
   const blockNumber = await watcher.getFinalizedBlockNumber();
-  expect(blockNumber).toBeGreaterThan(initialOptimismBlock);
+  console.log('blockNumber', blockNumber);
+  expect(blockNumber).toBeGreaterThan(105235062);
 });
 
 test('getMessagesForBlocks', async () => {
-  const watcher = new OptimismWatcher();
-  const vaasByBlock = await watcher.getMessagesForBlocks(69401778, 69401788);
+  const watcher = new EVMWatcher('optimism');
+  const vaasByBlock = await watcher.getMessagesForBlocks(105235070, 105235080);
   expect(vaasByBlock).toMatchObject({
-    '69401778/2023-01-20T23:56:40.000Z': [],
-    '69401779/2023-01-20T23:56:40.000Z': [
-      '0x5d3acc9f793afcd9ff97f569167222fae548eb354b1a6a685e5775e9a3530ff6:24/000000000000000000000000e2e2d9e31d7e1cc1178fe0d1c5950f6c809816a3/0',
-    ],
-    '69401780/2023-01-20T23:56:40.000Z': [],
-    '69401781/2023-01-20T23:56:40.000Z': [],
-    '69401782/2023-01-20T23:56:40.000Z': [],
-    '69401783/2023-01-20T23:56:40.000Z': [],
-    '69401784/2023-01-20T23:56:40.000Z': [],
-    '69401785/2023-01-20T23:56:40.000Z': [],
-    '69401786/2023-01-20T23:56:40.000Z': [],
-    '69401787/2023-01-20T23:56:40.000Z': [],
-    '69401788/2023-01-20T23:56:40.000Z': [],
+    '105235070/2023-06-06T16:28:37.000Z': [],
+    '105235071/2023-06-06T16:28:39.000Z': [],
+    '105235072/2023-06-06T16:28:41.000Z': [],
+    '105235073/2023-06-06T16:28:43.000Z': [],
+    '105235074/2023-06-06T16:28:45.000Z': [],
+    '105235075/2023-06-06T16:28:47.000Z': [],
+    '105235076/2023-06-06T16:28:49.000Z': [],
+    '105235077/2023-06-06T16:28:51.000Z': [],
+    '105235078/2023-06-06T16:28:53.000Z': [],
+    '105235079/2023-06-06T16:28:55.000Z': [],
+    '105235080/2023-06-06T16:28:57.000Z': [],
   });
 });

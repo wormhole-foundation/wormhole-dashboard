bb569b752bcc997d67fb837435d2b38692002305
Kevin Peters <kpeters@jumptrading.com>
2023-07-13 15:22:43 +0000

    fly reduce network egress changes

--- fly/cmd/track_pyth/main.go
+++ fly/cmd/track_pyth/main.go
@@ -260,7 +260,7 @@ func main() {
 
 	// Run supervisor.
 	supervisor.New(rootCtx, logger, func(ctx context.Context) error {
-		if err := supervisor.Run(ctx, "p2p", p2p.Run(obsvC, obsvReqC, nil, sendC, signedInC, priv, nil, gst, p2pPort, p2pNetworkID, p2pBootstrap, "", false, rootCtxCancel, nil, govConfigC, govStatusC)); err != nil {
+		if err := supervisor.Run(ctx, "p2p", p2p.Run(obsvC, obsvReqC, nil, sendC, signedInC, priv, nil, gst, p2pNetworkID, p2pBootstrap, "", false, rootCtxCancel, nil, nil, govConfigC, govStatusC, nil, nil)); err != nil {
 			return err
 		}
 

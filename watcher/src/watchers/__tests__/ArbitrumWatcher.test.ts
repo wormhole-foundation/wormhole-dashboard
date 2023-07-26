9e39ab8e58645e69a0372745cf6606266b36f21c
Paul Noel <panoel007@gmail.com>
2023-07-26 11:54:35 -0500

    watcher: add arbitrum test

--- watcher/src/watchers/__tests__/ArbitrumWatcher.test.ts
+++ watcher/src/watchers/__tests__/ArbitrumWatcher.test.ts
@@ -10,24 +10,27 @@ const initialEthBlock = Number(INITIAL_DEPLOYMENT_BLOCK_BY_CHAIN.ethereum);
 test('getFinalizedBlockNumber', async () => {
   const watcher = new ArbitrumWatcher();
   const blockNumber = await watcher.getFinalizedBlockNumber();
+  // The blockNumber is 0 because the most recent L2 block isn't in
+  // a finalized L1 block, yet.  It's in an L1 block.  That L1 block
+  // just isn't finalized, yet.  Now, if we had this test run for like
+  // 20 minutes, then we would get a non-zero result.
   expect(blockNumber).toEqual(0);
   let retval: number[] = watcher.getFirstMapEntry();
   expect(retval[0]).toBeGreaterThan(initialEthBlock);
   expect(retval[1]).toBeGreaterThan(initialArbitrumBlock);
 });
 
-// The following test will be enabled when there is a block to see.
-test.skip('getMessagesForBlocks', async () => {
+test('getMessagesForBlocks', async () => {
   const watcher = new ArbitrumWatcher();
-  const vaasByBlock = await watcher.getMessagesForBlocks(53473701, 53473701);
+  const vaasByBlock = await watcher.getMessagesForBlocks(114500582, 114500584);
   const entries = Object.entries(vaasByBlock);
-  expect(entries.length).toEqual(1);
-  expect(entries.filter(([block, vaas]) => vaas.length === 0).length).toEqual(1);
-  expect(entries.filter(([block, vaas]) => vaas.length === 1).length).toEqual(0);
+  expect(entries.length).toEqual(3);
+  expect(entries.filter(([block, vaas]) => vaas.length === 0).length).toEqual(2);
+  expect(entries.filter(([block, vaas]) => vaas.length === 1).length).toEqual(1);
   expect(entries.filter(([block, vaas]) => vaas.length === 2).length).toEqual(0);
-  expect(vaasByBlock['10974196/2023-01-06T04:23:21.045Z']).toBeDefined();
-  expect(vaasByBlock['10974196/2023-01-06T04:23:21.045Z'].length).toEqual(1);
-  expect(vaasByBlock['10974196/2023-01-06T04:23:21.045Z'][0]).toEqual(
-    '8A31CDE56ED3ACB7239D705949BD6C164747210A6C4C69D98756E0CF6D22C9EB:3/0000000000000000000000007cf7b764e38a0a5e967972c1df77d432510564e2/256813'
+  expect(vaasByBlock['114500583/2023-07-24T15:12:14.000Z']).toBeDefined();
+  expect(vaasByBlock['114500583/2023-07-24T15:12:14.000Z'].length).toEqual(1);
+  expect(vaasByBlock['114500583/2023-07-24T15:12:14.000Z'][0]).toEqual(
+    '0x39da3b500e5d65e82ca20cc8c4737fc0aa6c4e2c6c5f7e657834bd607c7109d9:23/0000000000000000000000000b2402144bb366a632d14b83f244d2e0e21bd39c/7628'
   );
 });

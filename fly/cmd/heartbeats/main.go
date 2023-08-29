b706bd4090c6b599527b3ab3522953906ed3ed31
Paul Noel <panoel007@gmail.com>
2023-08-29 13:09:37 -0500

    fly/cmd: add new tables

--- fly/cmd/heartbeats/main.go
+++ fly/cmd/heartbeats/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	tm "github.com/buger/goterm"
@@ -19,6 +21,7 @@ import (
 	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/wormhole-foundation/wormhole-monitor/fly/utils"
+	"github.com/wormhole-foundation/wormhole/sdk"
 	"github.com/wormhole-foundation/wormhole/sdk/vaa"
 
 	"go.uber.org/zap"
@@ -48,7 +51,77 @@ type heartbeat struct {
 	version       string
 }
 
+type obsvRateRow struct {
+	guardianIndex uint
+	guardianName  string
+	obsvCount     uint
+	percents      [10]string
+}
+
+var currentObsvData map[uint]uint
+var currentObsvTable map[uint]uint
+var obsvRateRows [numGuardians]obsvRateRow
+
+var knownEmitters map[string]bool // Holds the known token bridge emitters.
+
+// Guardian address to index map
+var guardianIndexMap = map[string]int{
+	strings.ToLower("0x58CC3AE5C097b213cE3c81979e1B9f9570746AA5"): 0,
+	strings.ToLower("0xfF6CB952589BDE862c25Ef4392132fb9D4A42157"): 1,
+	strings.ToLower("0x114De8460193bdf3A2fCf81f86a09765F4762fD1"): 2,
+	strings.ToLower("0x107A0086b32d7A0977926A205131d8731D39cbEB"): 3,
+	strings.ToLower("0x8C82B2fd82FaeD2711d59AF0F2499D16e726f6b2"): 4,
+	strings.ToLower("0x11b39756C042441BE6D8650b69b54EbE715E2343"): 5,
+	strings.ToLower("0x54Ce5B4D348fb74B958e8966e2ec3dBd4958a7cd"): 6,
+	strings.ToLower("0x15e7cAF07C4e3DC8e7C469f92C8Cd88FB8005a20"): 7,
+	strings.ToLower("0x74a3bf913953D695260D88BC1aA25A4eeE363ef0"): 8,
+	strings.ToLower("0x000aC0076727b35FBea2dAc28fEE5cCB0fEA768e"): 9,
+	strings.ToLower("0xAF45Ced136b9D9e24903464AE889F5C8a723FC14"): 10,
+	strings.ToLower("0xf93124b7c738843CBB89E864c862c38cddCccF95"): 11,
+	strings.ToLower("0xD2CC37A4dc036a8D232b48f62cDD4731412f4890"): 12,
+	strings.ToLower("0xDA798F6896A3331F64b48c12D1D57Fd9cbe70811"): 13,
+	strings.ToLower("0x71AA1BE1D36CaFE3867910F99C09e347899C19C3"): 14,
+	strings.ToLower("0x8192b6E7387CCd768277c17DAb1b7a5027c0b3Cf"): 15,
+	strings.ToLower("0x178e21ad2E77AE06711549CFBB1f9c7a9d8096e8"): 16,
+	strings.ToLower("0x5E1487F35515d02A92753504a8D75471b9f49EdB"): 17,
+	strings.ToLower("0x6FbEBc898F403E4773E95feB15E80C9A99c8348d"): 18,
+}
+
+var guardianIndexToNameMap = map[int]string{
+	0:  "Jump Crypto",
+	1:  "Staked",
+	2:  "Figment",
+	3:  "ChainodeTech",
+	4:  "Inotel",
+	5:  "HashQuark",
+	6:  "ChainLayer",
+	7:  "xLabs",
+	8:  "Forbole",
+	9:  "Staking Fund",
+	10: "MoonletWallet",
+	11: "P2P Validator",
+	12: "01node",
+	13: "MCF-V2-MAINNET",
+	14: "Everstake",
+	15: "Chorus One",
+	16: "syncnode",
+	17: "Triton",
+	18: "Staking Facilities",
+	19: "Totals:",
+}
+
+var lastTime = time.Now()
+
+const numGuardians = 19
+
 func main() {
+
+	// Fill in the known emitters
+	knownEmitters = make(map[string]bool)
+	for _, knownEmitter := range sdk.KnownEmitters {
+		knownEmitters[strings.ToLower(knownEmitter.Emitter)] = true
+	}
+	initObsvTableData(true)
 	// TODO: pass in config instead of hard-coding it
 	p2pNetworkID = "/wormhole/mainnet/2"
 	p2pBootstrap = "/dns4/wormhole-mainnet-v2-bootstrap.certus.one/udp/8999/quic/p2p/12D3KooWQp644DK27fd3d4Km3jr7gHiuJJ5ZGmy8hH4py7fP4FP7,/dns4/wormhole-v2-mainnet-bootstrap.xlabs.xyz/udp/8999/quic/p2p/12D3KooWNQ9tVrcb64tw6bNs2CaNrUGPM7yRrKvBBheQ5yCyPHKC"
@@ -83,6 +156,19 @@ func main() {
 	// Node's main lifecycle context.
 	rootCtx, rootCtxCancel = context.WithCancel(context.Background())
 	defer rootCtxCancel()
+
+	type GossipMsgType int16
+
+	const (
+		GSM_signedObservation GossipMsgType = iota
+		GSM_tbObservation
+		GSM_signedHeartbeat
+		GSM_signedVaaWithQuorum
+		GSM_signedObservationRequest
+		GSM_signedChainGovernorConfig
+		GSM_signedChainGovernorStatus
+		GSM_maxTypeVal
+	)
 
 	// Outbound gossip message queue
 	sendC := make(chan []byte)
@@ -121,7 +207,42 @@ func main() {
 
 	hbByGuardian := make(map[string]heartbeat, len(gs.Keys))
 
-	activeTable := 1 // 0 = chains, 1 = guardians
+	// Keep a counter of all gossip message types by guardian.
+	// Creating a 2 dimensional array:
+	// First dimension = guardian id
+	// Second dimension = gossip message type
+	// Value = count
+	const totalsRow = numGuardians
+	if len(gs.Keys) != numGuardians {
+		logger.Error("Invalid number of guardians.", zap.Int("found", len(gs.Keys)), zap.Uint32("expected", numGuardians))
+		return
+	}
+
+	var gossipLock sync.Mutex
+	// The extra row is for the totals
+	var gossipCounter = [numGuardians + 1][GSM_maxTypeVal]int{
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0}}
+
+	activeTable := 1 // 0 = chains, 1 = guardians, 2 = message counts, 3 = obsv rate
 
 	resetTerm(true)
 
@@ -132,6 +253,16 @@ func main() {
 	chainTable.SortBy([]table.SortBy{
 		{Name: "ID", Mode: table.AscNumeric},
 	})
+
+	gossipMsgTable := table.NewWriter()
+	gossipMsgTable.SetOutputMirror(os.Stdout)
+	gossipMsgTable.AppendHeader(table.Row{"#", "Guardian", "Obsv", "TB_OBsv", "HB", "VAA", "Obsv_Req", "Chain_Gov_Cfg", "Chain_Gov_Status"})
+	gossipMsgTable.SetStyle(table.StyleColoredDark)
+
+	obsvRateTable := table.NewWriter()
+	obsvRateTable.SetOutputMirror(os.Stdout)
+	obsvRateTable.AppendHeader(table.Row{"#", "Guardian", "Obsv", "1%", "2%", "3%", "4%", "5%", "6%", "7%", "8%", "9%", "10%"})
+	obsvRateTable.SetStyle(table.StyleColoredDark)
 
 	guardianTable := table.NewWriter()
 	guardianTable.SetOutputMirror(os.Stdout)
@@ -174,6 +305,16 @@ func main() {
 					resetTerm(true)
 					guardianTable.Render()
 					prompt()
+				case "m":
+					activeTable = 2
+					resetTerm(true)
+					gossipMsgTable.Render()
+					prompt()
+				case "o":
+					activeTable = 3
+					resetTerm(true)
+					obsvRateTable.Render()
+					prompt()
 				}
 			}
 			if wantsOut {
@@ -183,18 +324,40 @@ func main() {
 		rootCtxCancel()
 	}()
 
-	// Ignore observations
+	// Just count observations
 	go func() {
 		for {
 			select {
 			case <-rootCtx.Done():
 				return
-			case <-obsvC:
+			case o := <-obsvC:
+				spl := strings.Split(o.MessageId, "/")
+				emitter := strings.ToLower(spl[1])
+				addr := "0x" + string(hex.EncodeToString(o.Addr))
+				idx := guardianIndexMap[strings.ToLower(addr)]
+				if knownEmitters[emitter] {
+					gossipCounter[idx][GSM_tbObservation]++
+					gossipCounter[totalsRow][GSM_tbObservation]++
+				}
+				if handleObsv(uint(idx)) {
+					obsvRateTable.ResetRows()
+					for i := 0; i < numGuardians; i++ {
+						obsvRateTable.AppendRow(table.Row{i, obsvRateRows[int(i)].guardianName, obsvRateRows[int(i)].obsvCount, obsvRateRows[uint(i)].percents[0], obsvRateRows[uint(i)].percents[1], obsvRateRows[uint(i)].percents[2], obsvRateRows[uint(i)].percents[3], obsvRateRows[uint(i)].percents[4], obsvRateRows[uint(i)].percents[5], obsvRateRows[uint(i)].percents[6], obsvRateRows[uint(i)].percents[7], obsvRateRows[uint(i)].percents[8], obsvRateRows[uint(i)].percents[9]})
+					}
+				}
+				gossipCounter[idx][GSM_signedObservation]++
+				gossipCounter[totalsRow][GSM_signedObservation]++
+				gossipLock.Lock()
+				gossipMsgTable.ResetRows()
+				for idx, r := range gossipCounter {
+					gossipMsgTable.AppendRow(table.Row{idx, guardianIndexToNameMap[idx], r[0], r[1], r[2], r[3], r[4], r[5], r[6]})
+				}
+				gossipLock.Unlock()
 			}
 		}
 	}()
 
-	// Ignore observation requests
+	// Count observation requests
 	// Note: without this, the whole program hangs on observation requests
 	go func() {
 		for {
@@ -202,17 +365,34 @@ func main() {
 			case <-rootCtx.Done():
 				return
 			case <-obsvReqC:
+				// There is no guardian address in the observation request
+				// gossipCounter[idx][GSM_signedObservationRequest]++
+				gossipCounter[totalsRow][GSM_signedObservationRequest]++
+				gossipLock.Lock()
+				gossipMsgTable.ResetRows()
+				for idx, r := range gossipCounter {
+					gossipMsgTable.AppendRow(table.Row{idx, guardianIndexToNameMap[idx], r[0], r[1], r[2], r[3], r[4], r[5], r[6]})
+				}
+				gossipLock.Unlock()
 			}
 		}
 	}()
 
-	// Ignore signed VAAs
+	// Just count signed VAAs
 	go func() {
 		for {
 			select {
 			case <-rootCtx.Done():
 				return
 			case <-signedInC:
+				// This only has VAABytes. It doesn't have the guardian address
+				gossipCounter[totalsRow][GSM_signedVaaWithQuorum]++
+				gossipLock.Lock()
+				gossipMsgTable.ResetRows()
+				for idx, r := range gossipCounter {
+					gossipMsgTable.AppendRow(table.Row{idx, guardianIndexToNameMap[idx], r[0], r[1], r[2], r[3], r[4], r[5], r[6]})
+				}
+				gossipLock.Unlock()
 			}
 		}
 	}()
@@ -237,6 +417,9 @@ func main() {
 				}
 				chainTable.ResetRows()
 				guardianTable.ResetRows()
+				idx := guardianIndexMap[strings.ToLower(hb.GuardianAddr)]
+				gossipCounter[idx][GSM_signedHeartbeat]++
+				gossipCounter[totalsRow][GSM_signedHeartbeat]++
 				chainIdsToHeartbeats := make(map[uint32][]*gossipv1.Heartbeat_Network)
 				for idx, g := range gs.Keys {
 					info, ok := hbByGuardian[g.String()]
@@ -273,36 +456,68 @@ func main() {
 					}
 					chainTable.AppendRow(table.Row{chainId, vaa.ChainID(chainId), status, healthyCount, highest})
 				}
+				gossipLock.Lock()
+				gossipMsgTable.ResetRows()
+				for idx, r := range gossipCounter {
+					gossipMsgTable.AppendRow(table.Row{idx, guardianIndexToNameMap[idx], r[0], r[1], r[2], r[3], r[4], r[5], r[6]})
+				}
+				gossipLock.Unlock()
 				if activeTable == 0 {
 					resetTerm(false)
 					chainTable.Render()
-				} else {
+				} else if activeTable == 1 {
 					resetTerm(false)
 					guardianTable.Render()
+				} else if activeTable == 2 {
+					resetTerm(false)
+					gossipMsgTable.Render()
+				} else {
+					resetTerm(false)
+					obsvRateTable.Render()
 				}
 				prompt()
 			}
 		}
 	}()
 
-	// Handle govConfigs
+	// Count govConfigs
 	go func() {
 		for {
 			select {
 			case <-rootCtx.Done():
 				return
-			case <-govConfigC:
+			case g := <-govConfigC:
+				addr := "0x" + string(hex.EncodeToString(g.GuardianAddr))
+				idx := guardianIndexMap[strings.ToLower(addr)]
+				gossipCounter[idx][GSM_signedChainGovernorConfig]++
+				gossipCounter[totalsRow][GSM_signedChainGovernorConfig]++
+				gossipLock.Lock()
+				gossipMsgTable.ResetRows()
+				for idx, r := range gossipCounter {
+					gossipMsgTable.AppendRow(table.Row{idx, guardianIndexToNameMap[idx], r[0], r[1], r[2], r[3], r[4], r[5], r[6]})
+				}
+				gossipLock.Unlock()
 			}
 		}
 	}()
 
-	// Handle govStatus
+	// Count govStatus
 	go func() {
 		for {
 			select {
 			case <-rootCtx.Done():
 				return
-			case <-govStatusC:
+			case g := <-govStatusC:
+				addr := "0x" + string(hex.EncodeToString(g.GuardianAddr))
+				idx := guardianIndexMap[strings.ToLower(addr)]
+				gossipCounter[idx][GSM_signedChainGovernorStatus]++
+				gossipCounter[totalsRow][GSM_signedChainGovernorStatus]++
+				gossipLock.Lock()
+				gossipMsgTable.ResetRows()
+				for idx, r := range gossipCounter {
+					gossipMsgTable.AppendRow(table.Row{idx, guardianIndexToNameMap[idx], r[0], r[1], r[2], r[3], r[4], r[5], r[6]})
+				}
+				gossipLock.Unlock()
 			}
 		}
 	}()
@@ -341,6 +556,54 @@ func resetTerm(clear bool) {
 	tm.Flush()
 }
 
+func initObsvTableData(startup bool) {
+	if startup {
+		currentObsvData = make(map[uint]uint)
+		currentObsvTable = make(map[uint]uint)
+		for i := 0; i < numGuardians; i++ {
+			obsvRateRows[i].guardianIndex = uint(i)
+			obsvRateRows[i].guardianName = guardianIndexToNameMap[i]
+			obsvRateRows[i].obsvCount = 0
+			for j := 0; j < 10; j++ {
+				obsvRateRows[i].percents[j] = " "
+			}
+		}
+	}
+	for i := 0; i < numGuardians+1; i++ {
+		currentObsvData[uint(i)] = 0
+		if startup {
+			currentObsvTable[uint(i)] = 0
+		}
+	}
+}
+
+func handleObsv(idx uint) bool {
+	needToRender := false
+	now := time.Now()
+	if now.Sub(lastTime) > time.Minute {
+		lastTime = now
+		for idx, r := range currentObsvData {
+			currentObsvTable[idx] = r
+		}
+		for i := 0; i < numGuardians; i++ {
+			obsvRateRows[i].obsvCount = currentObsvTable[uint(i)]
+			pct := currentObsvTable[uint(i)] * 100 / currentObsvTable[numGuardians]
+			for j := 0; j < 10; j++ {
+				if pct >= uint(j+1) {
+					obsvRateRows[i].percents[j] = "="
+				} else {
+					obsvRateRows[i].percents[j] = " "
+				}
+			}
+		}
+		initObsvTableData(false)
+		needToRender = true
+	}
+	currentObsvData[idx]++
+	currentObsvData[numGuardians]++
+	return needToRender
+}
+
 func prompt() {
-	fmt.Print("[C]hains, [G]uardians, [Q]uit: ")
+	fmt.Print("[C]hains, [G]uardians, [M]essage Counts, [O]bsv Rate, [Q]uit: ")
 }
--- fly/go.mod
+++ fly/go.mod
@@ -1,6 +1,6 @@
 module github.com/wormhole-foundation/wormhole-monitor/fly
 
-go 1.19
+go 1.20
 
 require (
 	cloud.google.com/go/bigtable v1.18.1
@@ -9,12 +9,12 @@ require (
 	github.com/certusone/wormhole/node v0.0.0-20221110135008-bc1a740cfaa8
 	github.com/ethereum/go-ethereum v1.10.26
 	github.com/ipfs/go-log/v2 v2.5.1
-	github.com/libp2p/go-libp2p v0.22.0
+	github.com/libp2p/go-libp2p v0.27.0
 	github.com/mr-tron/base58 v1.2.0
 	github.com/wormhole-foundation/wormhole/sdk v0.0.0-00010101000000-000000000000
-	go.uber.org/zap v1.23.0
+	go.uber.org/zap v1.24.0
 	google.golang.org/api v0.110.0
-	google.golang.org/protobuf v1.28.1
+	google.golang.org/protobuf v1.30.0
 )
 
 require (
@@ -37,14 +37,15 @@ require (
 	github.com/cheekybits/genny v1.0.0 // indirect
 	github.com/cncf/udpa/go v0.0.0-20220112060539-c52dc94e7fbe // indirect
 	github.com/cncf/xds/go v0.0.0-20230105202645-06c439db220b // indirect
-	github.com/containerd/cgroups v1.0.4 // indirect
-	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/containerd/cgroups v1.1.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/davidlazar/go-crypto v0.0.0-20200604182044-b73af7476f6c // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
 	github.com/dgraph-io/badger/v3 v3.2103.1 // indirect
 	github.com/dgraph-io/ristretto v0.1.0 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/elastic/gosigar v0.14.2 // indirect
@@ -52,19 +53,23 @@ require (
 	github.com/envoyproxy/protoc-gen-validate v0.9.1 // indirect
 	github.com/flynn/noise v1.0.0 // indirect
 	github.com/francoispqt/gojay v1.2.13 // indirect
-	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.2.5 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gogo/protobuf v1.3.3 // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/flatbuffers v1.12.0 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gopacket v1.1.19 // indirect
+	github.com/google/pprof v0.0.0-20230405160723-4a4c7d95572b // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
@@ -72,102 +77,122 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3 // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d // indirect
-	github.com/huin/goupnp v1.0.3 // indirect
-	github.com/ipfs/go-cid v0.2.0 // indirect
-	github.com/ipfs/go-datastore v0.5.1 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.2 // indirect
+	github.com/huin/goupnp v1.1.0 // indirect
+	github.com/ipfs/boxo v0.8.0 // indirect
+	github.com/ipfs/go-cid v0.4.1 // indirect
+	github.com/ipfs/go-datastore v0.6.0 // indirect
 	github.com/ipfs/go-ipfs-util v0.0.2 // indirect
-	github.com/ipfs/go-ipns v0.2.0 // indirect
+	github.com/ipfs/go-ipns v0.3.0 // indirect
 	github.com/ipfs/go-log v1.0.5 // indirect
-	github.com/ipld/go-ipld-prime v0.9.0 // indirect
+	github.com/ipld/go-ipld-prime v0.20.0 // indirect
 	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
 	github.com/jbenet/go-temp-err-catcher v0.1.0 // indirect
 	github.com/jbenet/goprocess v0.1.4 // indirect
 	github.com/jedib0t/go-pretty/v6 v6.4.6 // indirect
-	github.com/klauspost/compress v1.15.11 // indirect
-	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
-	github.com/koron/go-ssdp v0.0.3 // indirect
+	github.com/klauspost/compress v1.16.4 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/koron/go-ssdp v0.0.4 // indirect
 	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
 	github.com/libp2p/go-cidranger v1.1.0 // indirect
 	github.com/libp2p/go-flow-metrics v0.1.0 // indirect
-	github.com/libp2p/go-libp2p-asn-util v0.2.0 // indirect
+	github.com/libp2p/go-libp2p-asn-util v0.3.0 // indirect
 	github.com/libp2p/go-libp2p-core v0.20.1 // indirect
-	github.com/libp2p/go-libp2p-kad-dht v0.18.0 // indirect
-	github.com/libp2p/go-libp2p-kbucket v0.4.7 // indirect
-	github.com/libp2p/go-libp2p-pubsub v0.8.0 // indirect
+	github.com/libp2p/go-libp2p-kad-dht v0.23.0 // indirect
+	github.com/libp2p/go-libp2p-kbucket v0.5.0 // indirect
+	github.com/libp2p/go-libp2p-pubsub v0.9.3 // indirect
 	github.com/libp2p/go-libp2p-record v0.2.0 // indirect
-	github.com/libp2p/go-msgio v0.2.0 // indirect
+	github.com/libp2p/go-msgio v0.3.0 // indirect
 	github.com/libp2p/go-nat v0.1.0 // indirect
-	github.com/libp2p/go-netroute v0.2.0 // indirect
+	github.com/libp2p/go-netroute v0.2.1 // indirect
 	github.com/libp2p/go-openssl v0.1.0 // indirect
 	github.com/libp2p/go-reuseport v0.2.0 // indirect
 	github.com/libp2p/go-yamux/v3 v3.1.2 // indirect
+	github.com/libp2p/go-yamux/v4 v4.0.0 // indirect
 	github.com/lucas-clemente/quic-go v0.28.1 // indirect
 	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
 	github.com/marten-seemann/qtls-go1-17 v0.1.2 // indirect
 	github.com/marten-seemann/qtls-go1-18 v0.1.2 // indirect
 	github.com/marten-seemann/qtls-go1-19 v0.1.0 // indirect
 	github.com/marten-seemann/tcp v0.0.0-20210406111302-dfbc87cc63fd // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-pointer v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/miekg/dns v1.1.50 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/miekg/dns v1.1.53 // indirect
 	github.com/mikioh/tcpinfo v0.0.0-20190314235526-30a79bb1804b // indirect
 	github.com/mikioh/tcpopt v0.0.0-20190314235656-172688c1accc // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
-	github.com/multiformats/go-base32 v0.0.4 // indirect
-	github.com/multiformats/go-base36 v0.1.0 // indirect
-	github.com/multiformats/go-multiaddr v0.6.0 // indirect
+	github.com/multiformats/go-base32 v0.1.0 // indirect
+	github.com/multiformats/go-base36 v0.2.0 // indirect
+	github.com/multiformats/go-multiaddr v0.9.0 // indirect
 	github.com/multiformats/go-multiaddr-dns v0.3.1 // indirect
 	github.com/multiformats/go-multiaddr-fmt v0.1.0 // indirect
-	github.com/multiformats/go-multibase v0.1.1 // indirect
-	github.com/multiformats/go-multicodec v0.5.0 // indirect
+	github.com/multiformats/go-multibase v0.2.0 // indirect
+	github.com/multiformats/go-multicodec v0.8.1 // indirect
 	github.com/multiformats/go-multihash v0.2.1 // indirect
-	github.com/multiformats/go-multistream v0.3.3 // indirect
-	github.com/multiformats/go-varint v0.0.6 // indirect
+	github.com/multiformats/go-multistream v0.4.1 // indirect
+	github.com/multiformats/go-varint v0.0.7 // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.2 // indirect
 	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/polydawn/refmt v0.0.0-20190807091052-3d65705ee9f1 // indirect
-	github.com/prometheus/client_golang v1.12.1 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.37.0 // indirect
-	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/polydawn/refmt v0.89.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/quic-go/qtls-go1-19 v0.3.2 // indirect
+	github.com/quic-go/qtls-go1-20 v0.2.2 // indirect
+	github.com/quic-go/quic-go v0.33.0 // indirect
+	github.com/quic-go/webtransport-go v0.5.2 // indirect
 	github.com/raulk/go-watchdog v1.3.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
 	github.com/whyrusleeping/go-keyspace v0.0.0-20160322163242-5b898ac5add1 // indirect
 	github.com/whyrusleeping/timecache v0.0.0-20160911033111-cfcb2f1abfee // indirect
 	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/otel v1.14.0 // indirect
+	go.opentelemetry.io/otel/trace v1.14.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/mod v0.7.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
+	go.uber.org/dig v1.16.1 // indirect
+	go.uber.org/fx v1.19.2 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.3.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	gonum.org/v1/gonum v0.11.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230222225845-10f96fb3dbec // indirect
 	google.golang.org/grpc v1.53.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.1.7 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
 
 // Needed for cosmos-sdk based chains.  See

67fd5f3933afead50049121da0c56867567df6f5
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-09-19 17:36:03 -0400

    commiting missing csvs

--- watcher/scripts/findSequenceGaps.ts
+++ watcher/scripts/findSequenceGaps.ts
@@ -3,81 +3,85 @@ import fs from 'fs';
 import axios from 'axios';
 
 // emitter => {sequence, timestamp}[]
-type SequenceByEmitter = Record<string, [bigint, number][]>;
-const URL = 'https://api.testnet.wormholescan.io/api/v1/vaas';
+type Seen = [bigint, number];
+type SequenceByEmitter = Record<string, Seen[]>;
+const URLS: string[] = [
+  'https://api.testnet.wormscan.io/api/v1/vaas',
+  // 'https://api.testnet.wormholescan.io/api/v1/vaas',
+];
 
 (async function () {
-  // For each chain
-  // page through VAAs until ts is > 4 weeks
-
-  // get 4 weeks ago timestamp
-  const until = new Date().getTime() - 4 * 7 * 24 * 60 * 60 * 1000;
-
+  const weeks = 8;
+  const until = new Date().getTime() - weeks * 7 * 24 * 60 * 60 * 1000;
+  const missing: string[] = [`chain_id/emitter/sequence,start_time,stop_time`];
+  // iterate through chains pulling n weeks of sequences, write to csv
   for (const cn of chains) {
     console.log('Working on: ', cn);
     const chain = chainToChainId.get(cn);
-    for (let i = 0; i < 5; i++) {
-      console.log(i);
-      const rows: string[] = [];
-      const sbe = await collectSeqences(chain!, until);
-      for (const emitter in sbe) {
-        const seqs = sbe[emitter];
-        for (const seq of seqs) {
-          rows.push(`${cn},${emitter},${seq[0]},${seqs[1]}`);
+    if (!chain) continue;
+
+    const sbe = await collectSeqences(chain!, until);
+    for (const emitter in sbe) {
+      const seqs = sbe[emitter];
+
+      for (let i = 0; i < seqs.length - 1; i++) {
+        const [newSeq, newTime] = seqs[i + 1]; // deeper is newer
+        const [oldSeq, oldTime] = seqs[i];
+        const delta = Number(newSeq - oldSeq);
+
+        if (delta === 1) continue;
+
+        for (let j = 1; j < delta; j++) {
+          const missingSeq = Number(oldSeq) + j;
+          missing.push(`${chain}/${emitter}/${missingSeq},${oldTime},${newTime}`);
         }
       }
-      fs.writeFileSync(`missing-seqs-${i}.csv`, rows.join('\n'));
     }
-
-    //// Find gaps and produce stats
-    //for (const emitter in sbe) {
-    //  const seqs = sbe[emitter];
-
-    //  for (let i = 0; i < seqs.length - 1; i++) {
-    //    const sd = Number(seqs[i + 1][0] - seqs[i][0]);
-    //    if (sd === 1) continue;
-
-    //    const avgTs = seqs[i + 1][1] + (seqs[i + 1][1] - seqs[i][1]) / 2;
-    //    rows.push(`${cn},${emitter},${avgTs},${seqs[i][0]},${sd}`);
-    //  }
-    //}
-    //fs.writeFileSync('missing-seqs.csv', rows.join('\n'));
-    return;
   }
+
+  fs.writeFileSync(`missing-seqs.csv`, missing.join('\n'));
 })();
 
 async function collectSeqences(chainId: number, until: number): Promise<SequenceByEmitter> {
   let sbe: SequenceByEmitter = {};
-  let minTs = new Date().getTime();
-  let page = 0;
-
   let seen: Record<string, boolean> = {};
 
-  while (minTs > until) {
-    const {
-      data: { data: vaas },
-    } = await axios.get(`${URL}/${chainId}?page=${page}&sort=DESC`);
+  for (const url of URLS) {
+    let page = 0;
+    let minTs = new Date().getTime();
+    while (minTs > until) {
+      const {
+        data: { data: vaas },
+      } = await axios.get(`${url}/${chainId}?page=${page}&pageSize=100`, {
+        // requied because the cache is busted as of writing
+        headers: { 'Cache-Control': 'no-cache, no-store' },
+      });
 
-    if (vaas.length === 0) break;
+      if (vaas.length === 0) break;
 
-    for (const vaa of vaas) {
-      // For some reason we get dupes?
-      if (vaa.id in seen) continue;
+      for (const vaa of vaas) {
+        if (vaa.id in seen) continue;
 
-      seen[vaa.id] = true;
+        seen[vaa.id] = true;
 
-      const emitter = vaa.emitterAddr;
-      const vaaTime = Date.parse(vaa.timestamp);
+        const seq = BigInt(vaa.sequence);
+        const emitter = vaa.emitterAddr;
+        const vaaTime = Date.parse(vaa.timestamp);
 
-      if (vaaTime < minTs) minTs = vaaTime;
-      if (!(emitter in sbe)) sbe[emitter] = [];
+        if (vaaTime < minTs) minTs = vaaTime;
+        if (minTs < until) break;
 
-      sbe[emitter].push([vaa.sequence, vaaTime]);
+        if (!(emitter in sbe)) sbe[emitter] = [];
+
+        sbe[emitter].push([seq, vaaTime / 1000]);
+      }
+
+      page += 1;
+      await new Promise((r) => setTimeout(r, 500));
     }
-
-    page += 1;
   }
 
+  // sort by sequence
   for (const emitter in sbe) {
     sbe[emitter] = sbe[emitter].sort((a: [bigint, number], b: [bigint, number]) =>
       Number(a[0] - b[0])

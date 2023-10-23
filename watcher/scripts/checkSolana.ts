96d3e22d8c6af0acad13a280e1e8f7ffb0d1cd25
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-10-23 12:33:31 -0400

    remove connect-sdk dep, restore typescript version

--- watcher/scripts/checkSolana.ts
+++ watcher/scripts/checkSolana.ts
@@ -2,12 +2,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { SolanaWatcher } from '../src/watchers/SolanaWatcher';
-import { CONFIG } from '@wormhole-foundation/connect-sdk';
 
 // Temporary script to test SolanaWatcher for lookup addresses
 (async () => {
-  const conf = CONFIG.Testnet.chains.Solana!;
-  const sw = new SolanaWatcher(conf.rpc, conf.contracts.coreBridge);
+  const sw = new SolanaWatcher();
   const msgs = await sw.getMessagesForBlocks(245230133, 245230333);
   console.log(msgs);
 })();

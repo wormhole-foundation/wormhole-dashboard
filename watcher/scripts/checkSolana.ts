2d5f0ae3b767f34270fe87e1aad1b844f8779504
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-10-23 12:29:12 -0400

    allow either 01 or 08 instruction ids

--- watcher/scripts/checkSolana.ts
+++ watcher/scripts/checkSolana.ts
@@ -7,9 +7,7 @@ import { CONFIG } from '@wormhole-foundation/connect-sdk';
 // Temporary script to test SolanaWatcher for lookup addresses
 (async () => {
   const conf = CONFIG.Testnet.chains.Solana!;
-  console.log(conf);
   const sw = new SolanaWatcher(conf.rpc, conf.contracts.coreBridge);
   const msgs = await sw.getMessagesForBlocks(245230133, 245230333);
-
   console.log(msgs);
 })();

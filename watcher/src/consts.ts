04086f772b79d2da5fdabddfd0f064341b6e0aa0
Paul Noel <panoel007@gmail.com>
2023-12-08 09:17:23 -0600

    watcher: update guardian rpc hosts

--- watcher/src/consts.ts
+++ watcher/src/consts.ts
@@ -84,7 +84,7 @@ export const AXIOS_CONFIG_JSON: AxiosRequestConfig = {
 };
 
 export const GUARDIAN_RPC_HOSTS = [
-  'https://wormhole-v2-mainnet-api.certus.one',
+  'https://api.wormholescan.io',
   'https://wormhole-v2-mainnet-api.mcf.rocks',
   'https://wormhole-v2-mainnet-api.chainlayer.network',
   'https://wormhole-v2-mainnet-api.staking.fund',

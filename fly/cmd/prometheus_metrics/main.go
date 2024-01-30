d95334ff4a4dca5472924b7c17802128d31eaf02
bingyuyap <bingyu.yap.21@gmail.com>
2024-01-31 04:23:48 +0800

    fly: move guardian name resolver to common
    Signed-off-by: bingyuyap <bingyu.yap.21@gmail.com>

--- fly/cmd/prometheus_metrics/main.go
+++ fly/cmd/prometheus_metrics/main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/certusone/wormhole/node/pkg/common"
+	node_common "github.com/certusone/wormhole/node/pkg/common"
 	"github.com/certusone/wormhole/node/pkg/p2p"
 	gossipv1 "github.com/certusone/wormhole/node/pkg/proto/gossip/v1"
 	"github.com/certusone/wormhole/node/pkg/supervisor"
@@ -18,6 +18,7 @@ import (
 	ipfslog "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/wormhole-foundation/wormhole-monitor/fly/common"
 	"github.com/wormhole-foundation/wormhole-monitor/fly/utils"
 	"github.com/wormhole-foundation/wormhole/sdk/vaa"
 	"go.uber.org/zap"
@@ -35,52 +36,6 @@ var (
 	nodeKeyPath  string
 	logLevel     string
 )
-
-// Guardian address to index map
-var guardianIndexMap = map[string]int{
-	strings.ToLower("0x58CC3AE5C097b213cE3c81979e1B9f9570746AA5"): 0,
-	strings.ToLower("0xfF6CB952589BDE862c25Ef4392132fb9D4A42157"): 1,
-	strings.ToLower("0x114De8460193bdf3A2fCf81f86a09765F4762fD1"): 2,
-	strings.ToLower("0x107A0086b32d7A0977926A205131d8731D39cbEB"): 3,
-	strings.ToLower("0x8C82B2fd82FaeD2711d59AF0F2499D16e726f6b2"): 4,
-	strings.ToLower("0x11b39756C042441BE6D8650b69b54EbE715E2343"): 5,
-	strings.ToLower("0x54Ce5B4D348fb74B958e8966e2ec3dBd4958a7cd"): 6,
-	strings.ToLower("0x15e7cAF07C4e3DC8e7C469f92C8Cd88FB8005a20"): 7,
-	strings.ToLower("0x74a3bf913953D695260D88BC1aA25A4eeE363ef0"): 8,
-	strings.ToLower("0x000aC0076727b35FBea2dAc28fEE5cCB0fEA768e"): 9,
-	strings.ToLower("0xAF45Ced136b9D9e24903464AE889F5C8a723FC14"): 10,
-	strings.ToLower("0xf93124b7c738843CBB89E864c862c38cddCccF95"): 11,
-	strings.ToLower("0xD2CC37A4dc036a8D232b48f62cDD4731412f4890"): 12,
-	strings.ToLower("0xDA798F6896A3331F64b48c12D1D57Fd9cbe70811"): 13,
-	strings.ToLower("0x71AA1BE1D36CaFE3867910F99C09e347899C19C3"): 14,
-	strings.ToLower("0x8192b6E7387CCd768277c17DAb1b7a5027c0b3Cf"): 15,
-	strings.ToLower("0x178e21ad2E77AE06711549CFBB1f9c7a9d8096e8"): 16,
-	strings.ToLower("0x5E1487F35515d02A92753504a8D75471b9f49EdB"): 17,
-	strings.ToLower("0x6FbEBc898F403E4773E95feB15E80C9A99c8348d"): 18,
-}
-
-var guardianIndexToNameMap = map[int]string{
-	0:  "Jump Crypto",
-	1:  "Staked",
-	2:  "Figment",
-	3:  "ChainodeTech",
-	4:  "Inotel",
-	5:  "HashQuark",
-	6:  "ChainLayer",
-	7:  "xLabs",
-	8:  "Forbole",
-	9:  "Staking Fund",
-	10: "MoonletWallet",
-	11: "P2P Validator",
-	12: "01node",
-	13: "MCF-V2-MAINNET",
-	14: "Everstake",
-	15: "Chorus One",
-	16: "syncnode",
-	17: "Triton",
-	18: "Staking Facilities",
-	19: "Totals:",
-}
 
 var guardianObservations = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
@@ -115,7 +70,7 @@ func initPromScraper(promRemoteURL *string, logger *zap.Logger) {
 
 		promLogger := logger.With(zap.String("component", "prometheus_scraper"))
 		errC := make(chan error)
-		common.StartRunnable(rootCtx, errC, false, "prometheus_scraper", func(ctx context.Context) error {
+		node_common.StartRunnable(rootCtx, errC, false, "prometheus_scraper", func(ctx context.Context) error {
 			t := time.NewTicker(15 * time.Second)
 
 			for {
@@ -188,7 +143,7 @@ func main() {
 	sendC := make(chan []byte)
 
 	// Inbound observations
-	obsvC := make(chan *common.MsgWithTimeStamp[gossipv1.SignedObservation], 1024)
+	obsvC := make(chan *node_common.MsgWithTimeStamp[gossipv1.SignedObservation], 1024)
 
 	// Inbound observation requests
 	obsvReqC := make(chan *gossipv1.ObservationRequest, 50)
@@ -200,7 +155,7 @@ func main() {
 	heartbeatC := make(chan *gossipv1.Heartbeat, 50)
 
 	// Guardian set state managed by processor
-	gst := common.NewGuardianSetState(heartbeatC)
+	gst := node_common.NewGuardianSetState(heartbeatC)
 
 	// Governor cfg
 	govConfigC := make(chan *gossipv1.SignedChainGovernorConfig, 50)
@@ -213,7 +168,7 @@ func main() {
 		logger.Fatal("Failed to fetch guardian set", zap.Error(err))
 	}
 	logger.Info("guardian set", zap.Uint32("index", idx), zap.Any("gs", sgs))
-	gs := common.GuardianSet{
+	gs := node_common.GuardianSet{
 		Keys:  sgs.Keys,
 		Index: idx,
 	}
@@ -239,7 +194,7 @@ func main() {
 						panic(err)
 					}
 					chainName := vaa.ChainID(ui64).String()
-					guardianName, ok := guardianIndexToNameMap[guardianIndexMap[strings.ToLower(ga)]]
+					guardianName, ok := common.GetGuardianName(ga)
 					if !ok {
 						logger.Error("guardian name not found", zap.String("guardian", ga))
 					}
@@ -280,10 +235,15 @@ func main() {
 				return
 			case hb := <-heartbeatC:
 				for _, network := range hb.Networks {
+					guardianName, ok := common.GetGuardianName(hb.GuardianAddr)
+					if !ok {
+						logger.Error("guardian name not found", zap.String("guardian", hb.GuardianAddr))
+					}
+
 					guardianChainHeight.With(
 						prometheus.Labels{
-							"guardian": guardianIndexToNameMap[guardianIndexMap[strings.ToLower(hb.GuardianAddr)]],
-							"chain":            vaa.ChainID(network.Id).String(),
+							"guardian": guardianName,
+							"chain":    vaa.ChainID(network.Id).String(),
 						},
 					).Set(float64(network.Height))
 				}
@@ -315,7 +275,7 @@ func main() {
 
 	// Load p2p private key
 	var priv crypto.PrivKey
-	priv, err = common.GetOrCreateNodeKey(logger, nodeKeyPath)
+	priv, err = node_common.GetOrCreateNodeKey(logger, nodeKeyPath)
 	if err != nil {
 		logger.Fatal("Failed to load node key", zap.Error(err))
 	}
---
+++ fly/common/getGuardianName.go
@@ -0,0 +1,54 @@
+package common
+
+import "strings"
+
+// Guardian address to index map
+var guardianIndexMap = map[string]int{
+	strings.ToLower("0x58CC3AE5C097b213cE3c81979e1B9f9570746AA5"): 0,
+	strings.ToLower("0xfF6CB952589BDE862c25Ef4392132fb9D4A42157"): 1,
+	strings.ToLower("0x114De8460193bdf3A2fCf81f86a09765F4762fD1"): 2,
+	strings.ToLower("0x107A0086b32d7A0977926A205131d8731D39cbEB"): 3,
+	strings.ToLower("0x8C82B2fd82FaeD2711d59AF0F2499D16e726f6b2"): 4,
+	strings.ToLower("0x11b39756C042441BE6D8650b69b54EbE715E2343"): 5,
+	strings.ToLower("0x54Ce5B4D348fb74B958e8966e2ec3dBd4958a7cd"): 6,
+	strings.ToLower("0x15e7cAF07C4e3DC8e7C469f92C8Cd88FB8005a20"): 7,
+	strings.ToLower("0x74a3bf913953D695260D88BC1aA25A4eeE363ef0"): 8,
+	strings.ToLower("0x000aC0076727b35FBea2dAc28fEE5cCB0fEA768e"): 9,
+	strings.ToLower("0xAF45Ced136b9D9e24903464AE889F5C8a723FC14"): 10,
+	strings.ToLower("0xf93124b7c738843CBB89E864c862c38cddCccF95"): 11,
+	strings.ToLower("0xD2CC37A4dc036a8D232b48f62cDD4731412f4890"): 12,
+	strings.ToLower("0xDA798F6896A3331F64b48c12D1D57Fd9cbe70811"): 13,
+	strings.ToLower("0x71AA1BE1D36CaFE3867910F99C09e347899C19C3"): 14,
+	strings.ToLower("0x8192b6E7387CCd768277c17DAb1b7a5027c0b3Cf"): 15,
+	strings.ToLower("0x178e21ad2E77AE06711549CFBB1f9c7a9d8096e8"): 16,
+	strings.ToLower("0x5E1487F35515d02A92753504a8D75471b9f49EdB"): 17,
+	strings.ToLower("0x6FbEBc898F403E4773E95feB15E80C9A99c8348d"): 18,
+}
+
+var guardianIndexToNameMap = map[int]string{
+	0:  "Jump Crypto",
+	1:  "Staked",
+	2:  "Figment",
+	3:  "ChainodeTech",
+	4:  "Inotel",
+	5:  "HashQuark",
+	6:  "ChainLayer",
+	7:  "xLabs",
+	8:  "Forbole",
+	9:  "Staking Fund",
+	10: "MoonletWallet",
+	11: "P2P Validator",
+	12: "01node",
+	13: "MCF-V2-MAINNET",
+	14: "Everstake",
+	15: "Chorus One",
+	16: "syncnode",
+	17: "Triton",
+	18: "Staking Facilities",
+	19: "Totals:",
+}
+
+func GetGuardianName(addr string) (string, bool) {
+	name, ok := guardianIndexToNameMap[guardianIndexMap[strings.ToLower(addr)]]
+	return name, ok
+}

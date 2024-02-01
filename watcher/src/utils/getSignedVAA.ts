e8fb8753a4cbdb70ed1aeaf271d4ebfd8601e2bd
Paul Noel <panoel007@gmail.com>
2024-02-01 16:14:30 -0600

    watcher: more splitting out of testnet

--- watcher/src/utils/getSignedVAA.ts
+++ watcher/src/utils/getSignedVAA.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { AXIOS_CONFIG_JSON, GUARDIAN_RPC_HOSTS } from '../consts';
+import { Environment, getEnvironment } from '@wormhole-foundation/wormhole-monitor-common';
 
 export const getSignedVAA = async (
   chain: number,
   emitter: string,
   sequence: string
 ): Promise<Buffer | null> => {
-  for (const host of GUARDIAN_RPC_HOSTS) {
+  const environment: Environment = getEnvironment();
+  for (const host of GUARDIAN_RPC_HOSTS[environment]) {
     try {
       const result = await axios.get(
         `${host}/v1/signed_vaa/${chain}/${emitter}/${sequence.toString()}`,

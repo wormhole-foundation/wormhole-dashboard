174b905a4965a0baa3462ef55a51bf771eb833a9
Paul Noel <panoel007@gmail.com>
2024-03-27 09:31:22 -0500

    watcher: add supervisor to monitor watchers

---
+++ watcher/src/workers/supervisor.ts
@@ -0,0 +1,63 @@
+import { ChainName } from '@certusone/wormhole-sdk';
+import { Worker } from 'worker_threads';
+import { HB_INTERVAL, WorkerData } from '../consts';
+import { getLogger } from '../utils/logger';
+import { Environment, getEnvironment } from '@wormhole-foundation/wormhole-monitor-common';
+
+interface WorkerInfo {
+  worker: Worker;
+  data: WorkerData;
+  lastHB: number;
+}
+
+const workers: { [key: string]: WorkerInfo } = {};
+const logger = getLogger('supervisor');
+const network: Environment = getEnvironment();
+
+function spawnWorker(data: WorkerData) {
+  const workerName = `${data.chain}Worker`;
+  logger.info(`Spawning worker ${workerName} on network ${network}...`);
+  const worker = new Worker('./dist/src/workers/worker.js', { workerData: data });
+
+  worker.on('message', (message) => {
+    if (message === 'heartbeat') {
+      logger.debug(`Worker ${workerName} sent HB`);
+      workers[workerName].lastHB = Date.now();
+    }
+  });
+
+  worker.on('exit', (code) => {
+    logger.warn(`Worker ${workerName} exited with code ${code}`);
+    if (code !== 0) {
+      logger.error(`Restarting worker ${workerName}...`);
+      spawnWorker(data);
+    }
+  });
+
+  workers[workerName] = { worker, data, lastHB: Date.now() };
+  logger.debug('Finished spawning worker:', workerName);
+}
+
+function monitorWorkers() {
+  setInterval(() => {
+    for (const [workerName, workerInfo] of Object.entries(workers)) {
+      logger.debug(
+        `Checking worker ${workerName} with lastHB of ${new Date(workerInfo.lastHB)}...`
+      );
+      if (Date.now() - workerInfo.lastHB > HB_INTERVAL) {
+        logger.error(`Worker ${workerName} missed HB, restarting...`);
+        workerInfo.worker.terminate();
+        spawnWorker(workerInfo.data);
+      }
+    }
+  }, HB_INTERVAL);
+}
+
+export function startSupervisor(supportedChains: ChainName[]) {
+  supportedChains.forEach((chain) => {
+    const workerData: WorkerData = { network, chain };
+    spawnWorker(workerData);
+  });
+
+  monitorWorkers();
+}

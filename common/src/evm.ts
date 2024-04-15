a1298a9b0818b63c18380cd1b29af91386c310c5
bingyuyap <bingyu.yap.21@gmail.com>
2024-04-15 12:44:57 +0800

    cloud_functions: get ntt rate limits
    Signed-off-by: bingyuyap <bingyu.yap.21@gmail.com>

---
+++ common/src/evm.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { keccak256 } from '@wormhole-foundation/sdk-definitions';
+
+export async function callContractMethod(
+  rpc: string,
+  contractAddress: string,
+  methodId: string,
+  params: string = ''
+): Promise<string> {
+  const data = methodId + params;
+
+  const payload = {
+    jsonrpc: '2.0',
+    method: 'eth_call',
+    params: [
+      {
+        to: contractAddress,
+        data,
+      },
+      'latest',
+    ],
+    id: 1,
+  };
+
+  try {
+    const response = await axios.post(rpc, payload);
+    if (response.data.error) {
+      throw new Error(`Error calling contract method: ${response.data.error.message}`);
+    }
+    return response.data.result;
+  } catch (e) {
+    throw new Error(`Failed to call contract method: ${e}`);
+  }
+}
+
+export function getMethodId(methodSignature: string): string {
+  const hashArray: Uint8Array = keccak256(methodSignature);
+  const hashHex = Array.from(hashArray)
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
+  return '0x' + hashHex.substring(0, 8);
+}
+
+export async function getEvmTokenDecimals(rpc: string, contractAddress: string): Promise<number> {
+  const methodId = getMethodId('tokenDecimals()');
+  const result = await callContractMethod(rpc, contractAddress, methodId);
+  return Number(result);
+}
--- dashboard/src/utils/evmNttHelpers.ts
+++ dashboard/src/utils/evmNttHelpers.ts
@@ -1,45 +1,7 @@
-import axios from 'axios';
-import { keccak256 } from '@wormhole-foundation/sdk-definitions';
-
-async function callContractMethod(
-  rpc: string,
-  contractAddress: string,
-  methodId: string,
-  params: string = ''
-): Promise<string> {
-  const data = methodId + params;
-
-  const payload = {
-    jsonrpc: '2.0',
-    method: 'eth_call',
-    params: [
-      {
-        to: contractAddress,
-        data,
-      },
-      'latest',
-    ],
-    id: 1,
-  };
-
-  try {
-    const response = await axios.post(rpc, payload);
-    if (response.data.error) {
-      throw new Error(`Error calling contract method: ${response.data.error.message}`);
-    }
-    return response.data.result;
-  } catch (e) {
-    throw new Error(`Failed to call contract method: ${e}`);
-  }
-}
-
-function getMethodId(methodSignature: string): string {
-  const hashArray: Uint8Array = keccak256(methodSignature);
-  const hashHex = Array.from(hashArray)
-    .map((b) => b.toString(16).padStart(2, '0'))
-    .join('');
-  return '0x' + hashHex.substring(0, 8);
-}
+import {
+  callContractMethod,
+  getMethodId,
+} from '@wormhole-foundation/wormhole-monitor-common';
 
 export async function getCurrentOutboundCapacity(
   rpc: string,
@@ -59,10 +21,4 @@ export async function getCurrentInboundCapacity(
   const chainIdHex = chainId.toString(16).padStart(64, '0');
   const result = await callContractMethod(rpc, contractAddress, methodId, chainIdHex);
   return BigInt(result);
-}
-
-export async function getTokenDecimals(rpc: string, contractAddress: string): Promise<number> {
-  const methodId = getMethodId('tokenDecimals()');
-  const result = await callContractMethod(rpc, contractAddress, methodId);
-  return Number(result);
 }

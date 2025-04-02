49f1886e624297ea9e988c70e326a34e70271acb
Paul Noel <panoel007@gmail.com>
2025-04-02 10:32:16 -0400

    cloud_functions: reobs uses solana hashes instead of accounts

--- cloud_functions/src/getReobserveVaas.ts
+++ cloud_functions/src/getReobserveVaas.ts
@@ -1,53 +1,13 @@
-import { Connection } from '@solana/web3.js';
-import { chainToChainId, contracts } from '@wormhole-foundation/sdk-base';
+import { chainToChainId } from '@wormhole-foundation/sdk-base';
 import {
   assertEnvironmentVariable,
   getNetwork,
-  isLegacyMessage,
   isVAASigned,
-  normalizeCompileInstruction,
   ReobserveInfo,
 } from '@wormhole-foundation/wormhole-monitor-common';
 import { Firestore } from 'firebase-admin/firestore';
 
 const MAX_VAAS_TO_REOBSERVE = 25;
-
-export async function convertSolanaTxToAccts(txHash: string): Promise<string[]> {
-  const POST_MESSAGE_IX_ID = 0x01;
-  let accounts: string[] = [];
-  const network = getNetwork();
-  const endpoint: string =
-    network === 'Mainnet' ? 'https://api.mainnet-beta.solana.com' : 'https://api.devnet.solana.com';
-  const connection = new Connection(endpoint, 'finalized');
-  const txs = await connection.getTransactions([txHash], {
-    maxSupportedTransactionVersion: 0,
-  });
-  for (const tx of txs) {
-    if (!tx) {
-      continue;
-    }
-    const message = tx.transaction.message;
-    const accountKeys = isLegacyMessage(message) ? message.accountKeys : message.staticAccountKeys;
-    const programIdIndex = accountKeys.findIndex(
-      (i) => i.toBase58() === contracts.coreBridge(network, 'Solana')
-    );
-    const instructions = message.compiledInstructions;
-    const innerInstructions =
-      tx.meta?.innerInstructions?.flatMap((i) => i.instructions.map(normalizeCompileInstruction)) ||
-      [];
-    const whInstructions = innerInstructions
-      .concat(instructions)
-      .filter((i) => i.programIdIndex === programIdIndex);
-    for (const instruction of whInstructions) {
-      // skip if not postMessage instruction
-      const instructionId = instruction.data;
-      if (instructionId[0] !== POST_MESSAGE_IX_ID) continue;
-
-      accounts.push(accountKeys[instruction.accountKeyIndexes[1]].toBase58());
-    }
-  }
-  return accounts;
-}
 
 export async function getReobserveVaas(req: any, res: any) {
   res.set('Access-Control-Allow-Origin', '*');
@@ -72,18 +32,9 @@ export async function getReobserveVaas(req: any, res: any) {
     res.sendStatus(500);
   }
 
-  let reobs: (ReobserveInfo[] | null)[] = [];
-  try {
-    const vaaArray = Array.from(reobsMap.values());
-    // Process each VAA asynchronously and filter out any null results
-    reobs = (await Promise.all(vaaArray.map(processVaa))).filter((vaa) => vaa !== null); // Remove any VAA that failed conversion
-  } catch (e) {
-    console.error('error processing reobservations', e);
-    console.error('reobs', reobs);
-    res.sendStatus(500);
-  }
+  const vaaArray = Array.from(reobsMap.values());
   // Need to flatten the array of arrays before returning
-  const retVal = reobs.flat();
+  const retVal = vaaArray.flat();
   res.status(200).send(JSON.stringify(retVal));
   return;
 }
@@ -142,25 +93,4 @@ async function getAndProcessReobsVAAs(): Promise<Map<string, ReobserveInfo>> {
   }
   console.log('number of reobservable VAAs that are not signed', current.size);
   return current;
-}
-
-async function processVaa(vaa: ReobserveInfo): Promise<ReobserveInfo[] | null> {
-  let vaas: ReobserveInfo[] = [];
-
-  if (vaa.chain === chainToChainId('Solana')) {
-    const origTxHash = vaa.txhash;
-    const convertedTxHash: string[] = await convertSolanaTxToAccts(origTxHash);
-    console.log(`Converted solana txHash ${origTxHash} to account ${convertedTxHash}`);
-
-    if (convertedTxHash.length === 0) {
-      console.error(`Failed to convert solana txHash ${origTxHash} to an account.`);
-      return null; // Indicate failure to convert
-    }
-    for (const account of convertedTxHash) {
-      vaas.push({ ...vaa, txhash: account }); // Return a new object with the updated txhash
-    }
-  } else {
-    vaas.push(vaa); // Return the original object for non-Solana chains
-  }
-  return vaas;
 }
